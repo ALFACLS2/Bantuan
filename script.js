8ae97c6fce42081b954ff007f9831b1d45de7846
ALFACLS2 <anggoro.sasmito@sat.co.id>
2026-03-28 16:00:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-function showPage(pageId) {
-  document.querySelectorAll('.page').forEach(page => {
-    page.classList.remove('active');
-  });
+async function loadFAQ(btn) {
+  btn.style.background = "linear-gradient(90deg, #ff7a18, #ff3d81)";
 
-  document.getElementById(pageId).classList.add('active');
+  setTimeout(async () => {
+    const res = await fetch("faq.html");
+    const html = await res.text();
+
+    const page = document.getElementById("faqPage");
+    page.innerHTML = html;
+    page.classList.add("active");
+
+  }, 200);
+}
+
+function closeFAQ() {
+  document.getElementById("faqPage").classList.remove("active");
 }
