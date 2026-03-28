f01184a44cebe4e6f44aadbb9c2c544f4ae3b395
ALFACLS2 <anggoro.sasmito@sat.co.id>
2026-03-28 16:30:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 async function loadFAQ() {
   const page = document.getElementById("faqPage");
 
-  try {
-    const res = await fetch("./faq.html");
-    const html = await res.text();
+  const res = await fetch("./faq.html?v=3");
+  const html = await res.text();
 
-    page.innerHTML = html;
-    page.classList.add("active");
-
-  } catch {
-    alert("FAQ gagal dimuat 😏");
-  }
+  page.innerHTML = html;
+  page.classList.add("active");
 }
 
 function closeFAQ() {
   const page = document.getElementById("faqPage");
-
   page.classList.remove("active");
 
   setTimeout(() => {
