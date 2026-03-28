085c94af2fe4b325bbe19f5315a2136a8638a77d
ALFACLS2 <anggoro.sasmito@sat.co.id>
2026-03-28 16:43:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+async function loadFAQ() {
+  const page = document.getElementById("faqPage");
+
+  const res = await fetch("./faq.html?v=10");
+  const html = await res.text();
+
+  page.innerHTML = html;
+  page.classList.add("active");
+}
+
+function closeFAQ() {
+  const page = document.getElementById("faqPage");
+  page.classList.remove("active");
+
+  setTimeout(() => {
+    page.innerHTML = "";
+  }, 300);
+}
+
 function toggleFAQ(el) {
   const item = el.parentElement;
-
   item.classList.toggle("active");
 }
